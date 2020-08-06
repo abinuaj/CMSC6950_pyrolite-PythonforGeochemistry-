7b207eb4b4eb842f3e5a8d0ce8d05551a0aae17b
Abinu Ajithan Jyothini <aajyothini@mun.ca>
2020-08-05 23:06:43 -0230

    added one more plot file

---
+++ gcplot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+from Bio import SeqIO
+from Bio.SeqUtils import GC 
+gc = sorted(GC(rec.seq) for rec in SeqIO.parse("fasta.fasta", "fasta"))
+plt.xlabel("Genes")
+plt.ylabel("GC Percentage")
+plt.grid()
+plt.plot(gc)
+plt.savefig("gcplot.png")
+
